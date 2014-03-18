af04d5c6e41413c8ca37b7e35ce771d9cb9b58dc
Mike Lang <mikelang3000@gmail.com>
2014-03-18 05:31:23 -0700

    gtb: Fix some spaces-indentation that snuck in

--- libs/gtb.py
+++ libs/gtb.py
@@ -12,30 +12,30 @@ def greenlet_tb(g):
 	"""Takes a greenlet and returns a traceback string, or None if not running.
 	Treats the currently active greenlet as a special case, but still returns the correct result.
 	"""
-    frame = g.gr_frame
+	frame = g.gr_frame
 	if not frame:
 		current = greenlet.getcurrent()
 		if g is not current:
 			return None # not running
 		# the current greenlet has no saved frame object, use traceback.format_stack instead.
 		return ''.join(traceback.format_stack())
-    stack = []
-    while frame:
-        filename = frame.f_code.co_filename
-        lineno = frame.f_lineno
-        funcname = frame.f_code.co_name
-        try:
-            line = open(filename).read().split('\n')[lineno-1].strip()
-        except (IOError, KeyError):
-            line = None
-        stack.append((filename, lineno, funcname, line))
-        frame = frame.f_back
-    stack = stack[::-1]
-    return ''.join(traceback.format_list(stack))
+	stack = []
+	while frame:
+		filename = frame.f_code.co_filename
+		lineno = frame.f_lineno
+		funcname = frame.f_code.co_name
+		try:
+			line = open(filename).read().split('\n')[lineno-1].strip()
+		except (IOError, KeyError):
+			line = None
+		stack.append((filename, lineno, funcname, line))
+		frame = frame.f_back
+	stack = stack[::-1]
+	return ''.join(traceback.format_list(stack))
 
 def get_greenlets():
 	"""Fetch all greenlet instances from the garbage collector"""
-    return [o for o in gc.get_objects() if isinstance(o, greenlet)]
+	return [o for o in gc.get_objects() if isinstance(o, greenlet)]
 
 def print_greenlet_tbs():
 	"""Print all greenlets along with a traceback, or a short diagnostic (eg. <started but not running>)."""
