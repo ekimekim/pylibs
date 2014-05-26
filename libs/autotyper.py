f3790077ee5936e0cf51ca222b7c2758f00f82e2
Mike Lang <mikelang3000@gmail.com>
2014-05-25 22:23:18 -0700

    autotyper: Options to make it type slower

--- libs/autotyper.py
+++ libs/autotyper.py
@@ -7,7 +7,12 @@ Requires the xte utility, provided by the "xautomation" package on most distribu
 import sys
 from subprocess import Popen, PIPE
 
+import scriptlib
+
 AI_HACK = True
+KEY_WAIT = True # sleep between every key? (otherwise, every command)
+INTERVAL = 10000 # us to sleep between keys
+DEBUG = False
 
 def to_commands(s):
 	ret = []
@@ -20,6 +25,10 @@ def to_commands(s):
 	for c in s:
 		if c in simple:
 			part += c
+			if KEY_WAIT:
+				# make end of part trigger early so each char is a seperate str command
+				ret.append("str {}".format(part))
+				part = ""
 		else:
 			if part:
 				ret.append("str {}".format(part))
@@ -35,7 +44,12 @@ def to_commands(s):
 
 def send_commands(commands):
 	proc = Popen(['xte'], stdin=PIPE)
-	data = '\n'.join(commands) + '\n'
+	if INTERVAL:
+		joinstr = '\nusleep {}\n'.format(INTERVAL)
+	else:
+		joinstr = '\n'
+	data = joinstr.join(commands) + '\n'
+	if DEBUG: print data
 	proc.communicate(data)
 
 
@@ -49,7 +63,13 @@ def from_file(path):
 	send(data)
 
 
-def main(*args):
+@scriptlib.with_argv
+def main(*args, **kwargs):
+	global INTERVAL, AI_HACK, DEBUG, KEY_WAIT
+	INTERVAL = kwargs.get("interval", INTERVAL)
+	AI_HACK = kwargs.get("ai-hack", AI_HACK)
+	DEBUG = kwargs.get("debug", DEBUG)
+	KEY_WAIT = kwargs.get("debug", KEY_WAIT)
 	for arg in args:
 		if arg == '-':
 			send(sys.stdin.read())
@@ -58,4 +78,4 @@ def main(*args):
 
 
 if __name__ == '__main__':
-	main(*sys.argv[1:])
+	main()
