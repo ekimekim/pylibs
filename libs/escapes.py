942f8312725a1601a5b798cf2ccac8a711bf5cec
Mike Lang <mikelang3000@gmail.com>
2014-07-05 20:19:06 -0700

    escapes: Some more extensive functionality and better layout

--- libs/escapes.py
+++ libs/escapes.py
@@ -1,22 +1,35 @@
 
-"""A collection of definitions of ANSI control codes for terminals"""
+"""A collection of definitions of ANSI control codes for terminals.
+Things defined here (or returned from functions) are strings that should be printed to the terminal.
+"""
 
 ESC = "\033" # escape character for terminal
 CSI = ESC + "[" # Control Sequence Initiator
 
+# Clearing
 CLEAR = CSI + "H" + CSI + "2J" # Clears screen. Returns cursor to (1,1).
-CLEAR_LINE = CSI + "2K"
+CLEAR_LINE = CSI + "2K" # Clears whole line, doesn't move cursor.
 
 # Cursor 
 SAVE_CURSOR = CSI + "s"
 LOAD_CURSOR = CSI + "u"
 TYPE_CURSOR = CSI + "6n" # Writes CSI <row> ";" <column> "R" to stdin
-SET_CURSOR = lambda x,y: CSI + str(y) + ";" + str(x) + "H" # Sets cursor to (column, row)
+def set_cursor(x=None, y=None):
+	"""Set cursor to (column, row). If either axis omitted or None, it doesn't move."""
+	tostr = lambda val: '' if val is None else str(val)
+	return "{CSI}{y};{x}H".format(CSI=CSI, x=x, y=y)
 
-CURSOR_UP = CSI + "A"
-CURSOR_DOWN = CSI + "B"
-CURSOR_RIGHT = CSI + "C"
-CURSOR_LEFT = CSI + "D"
+# Cursor Movement
+UP, DOWN, LEFT, RIGHT = "ABCD"
+def move_cursor(direction, steps=1):
+	"""Takes a direction UP, DOWN, LEFT or RIGHT, and the amount to move."""
+	return CSI + str(steps) + direction
+
+# These shortcuts are for simple use and backwards compatibility
+CURSOR_UP = move_cursor(UP)
+CURSOR_DOWN = move_cursor(DOWN)
+CURSOR_LEFT = move_cursor(LEFT)
+CURSOR_RIGHT = move_cursor(RIGHT)
 
 SCROLL_UP = CSI + "S"
 SCROLL_DOWN = CSI + "D"
