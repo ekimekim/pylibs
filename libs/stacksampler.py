28878291073e8a00ae473c5260f48a27cdbca854
Mike Lang <mikelang3000@gmail.com>
2020-02-21 22:21:34 -0800

    wip: only keep top n samples

--- libs/stacksampler.py
+++ libs/stacksampler.py
@@ -3,11 +3,22 @@ import atexit
 import signal
 from collections import Counter
 
-def install(filepath, interval=0.005):
-	"""Samples the stack every INTERVAL seconds of user time, outputing to filepath on SIGUSR1 or exit.
+def install(filepath=None, interval=0.005, prometheus_top_n=None):
+	"""Samples the stack every INTERVAL seconds of user time, outputing to filepath (if given) on SIGUSR1 or exit.
+	If prometheus_top_n > 0, report approximate time spent in up to prometheus_top_n unique stacks as prometheus
+	metric "stacksampler_user_time". If prometheus_top_n=0, all unique stacks are reported.
+	The limit is intended to avoid a large number of rare or irrelevant stacks from blowing out prom storage
+	or making scrapes take a long time.
 	We could use user+sys time but that leads to interrupting syscalls,
 	which may affect performance, and we care mostly about user time anyway.
 	"""
+	# Note we only start each next timer once the previous timer signal has been processed.
+	# There are two reasons for this:
+	# 1. Avoid handling a signal while already handling a signal, however unlikely,
+	#    as this could lead to a deadlock due to locking inside prometheus_client.
+	# 2. Avoid biasing the results by effectively not including the time taken to do the actual
+	#    stack sampling.
+
 	samples = Counter()
 
 	def sample(signum, frame):
@@ -30,9 +41,13 @@ def install(filepath, interval=0.005):
 		output()
 
 	def output(*args):
+		if filepath is None:
+			return
 		with open(filepath, 'w') as f:
 			for key, count in samples.items():
 				f.write("{} {}\n".format(key, count))
+
+	# TODO UPTO register collector for prom to report topn samples
 
 	atexit.register(finish)
 
@@ -40,4 +55,3 @@ def install(filepath, interval=0.005):
 	signal.signal(signal.SIGUSR1, output)
 	# deliver the first signal in INTERVAL seconds
 	signal.setitimer(signal.ITIMER_VIRTUAL, interval)
-
