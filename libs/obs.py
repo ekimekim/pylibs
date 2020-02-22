4ab5b37a0347f1f2de8534a0fdaf7c47b2628a42
Mike Lang <mikelang3000@gmail.com>
2020-02-21 23:42:47 -0800

    wip:

--- libs/obs.py
+++ libs/obs.py
@@ -14,62 +14,35 @@ import obspython
 
 
 def unload():
-	for resource in Resource.instances:
-		resource.release()
+	for releasable in Releasable.instances:
+		releasable.release()
 
 
-class Resource(object):
-	"""Common code for all resource-like objects
+class Releasable(object):
+	"""Mixin for objects that need to be released upon destruction
+	or unload. Can also be explicitly released with release()
+	or on exit when used as a context manager.
 
-	Call obs methods for the resource easily:
-		resource.method(arg) -> obspython.obs_TYPE_METHOD(resource, arg)
-
-	Automatically releases the reference (if ref counted) on GC or unload().
-	Can explicitly release with resource.release() or on exit if used as a context manager.
+	Can check if already released with 'released' property.
 	"""
 
-	# Track all Resource instances with a weak reference so we can find them from unload()
+	# Track all Releasable instances with a weak reference so we can find them from unload()
 	instances = weakref.WeakSet()
 
-	# The name as used in obs methods, eg. obs_NAME_release()
-	# Leave none to default to cls.__name__.lower().
-	type_name = None
+	released = False
 
-	def __init__(self, reference):
-		self._reference = reference
+	def __init__(self):
 		self.instances.add(self)
-		if self.type_name is None:
-			self.type_name = type(self).__name__.lower()
-
-	@property
-	def reference(self):
-		if self._reference is None:
-			raise Exception(f"Reference to {self.type_name} used after being released")
-		return self._reference
-
-	# Override name property to customise __str__ representation,
-	# defaults to just the reference __str__
-	@property
-	def name(self):
-		return self._reference
-
-	def __str__(self):
-		return f"<{type(self).__name__} {self.name}>"
-	__repr__ = __str__
 
 	def release(self):
-		if self._reference is not None:
-			self.get_method('release')()
-			self._reference = None
+		if self.released:
+			return
+		self._release()
+		self.released = True
 
-	def __getattr__(self, attr):
-		return self.get_method(attr)
-
-	def get_method(self, name):
-		method = getattr(obspython, f'obs_{self.type_name}_{name}')
-		def bound_method(*args):
-			return method(self.reference, *args)
-		return bound_method
+	def _release(self):
+		"""Actually release the object"""
+		raise NotImplementedError
 
 	def __del__(self):
 		self.release()
@@ -79,6 +52,76 @@ class Resource(object):
 
 	def __exit__(self, *exc_info):
 		self.release()
+
+
+class Timer(Releasable):
+	"""Repeating timer that triggers callback every INTERVAL seconds.
+	Can also operate in a one-shot mode if repeat=False.
+	"""
+	def __init__(self, callback, interval, repeat=True):
+		# OBS uses the python callable object as the timer id, so
+		# we create a unique callable.
+		def run():
+			callback()
+			if not repeat:
+				self.release()
+		self.run = run
+		obspython.timer_add(self.run, int(interval * 1000))
+
+	def _release(self):
+		obspython.timer_remove(self.run)
+
+
+class Resource(Releasable):
+	"""Common code for all resource-like objects
+
+	Call obs methods for the resource easily:
+		resource.method(arg) -> obspython.obs_TYPE_METHOD(resource, arg)
+
+	Automatically releases the reference (if ref counted) on GC or unload().
+	Can explicitly release with resource.release() or on exit if used as a context manager.
+	"""
+
+	# The name as used in obs methods, eg. obs_NAME_release()
+	# Leave none to default to cls.__name__.lower().
+	type_name = None
+
+	def __init__(self, reference):
+		self._reference = reference
+		if self.type_name is None:
+			self.type_name = type(self).__name__.lower()
+		super().__init__()
+
+	@property
+	def reference(self):
+		if self.released:
+			raise Exception(f"Reference to {self.type_name} used after being released")
+		return self._reference
+
+	# Override name property to customise __str__ representation,
+	# defaults to just the reference __str__.
+	@property
+	def name(self):
+		return self._reference
+
+	def __str__(self):
+		if self.released:
+			return f"<{type(self).__name__} (RELEASED)>"
+		else:
+			return f"<{type(self).__name__} {self.name}>"
+	__repr__ = __str__
+
+	def _release(self):
+		self.get_method('release')()
+
+	def __getattr__(self, attr):
+		return self.get_method(attr)
+
+	def get_method(self, name):
+		method = getattr(obspython, f'obs_{self.type_name}_{name}')
+		def bound_method(*args):
+			return method(self.reference, *args)
+		return bound_method
 
 
 class Source(Resource):
